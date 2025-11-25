f615c06bf060339748cf85ac7cdcf2758b53ac9a
Jathusan Jesuthasan <159249503+Jathusan-Jesuthasan@users.noreply.github.com>
2025-11-25 11:43:23 +0530

    UI polished ViewProject.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Navbar from "./components/Navbar";
 import Hero from "./components/hero.jsx";
